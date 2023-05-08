ca73c8fccf2360770f1f741bdf9478ae8299cf15
egert97 <74450079+egert97@users.noreply.github.com>
2023-05-08 14:37:00 +0300

    heatmap
    added heatmap.html, edited heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,17 +1,34 @@
-addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
-// add geoJSON layer
-async function addGeoJson(url) {
- const response = await fetch(url)
- const data = await response.json()
- const heatData = data.features.map(heatDataConvert)
- const heatMap = L.heatLayer(heatData, { radius: 10 })
- heatMap.addTo(map)
+var map = L.map('map').setView([58.3781, 26.7295], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  maxZoom: 19
+}).addTo(map);
+
+var cellTowers = L.geoJSON(tartu_city_celltowers_edu, {
+  pointToLayer: function (feature, latlng) {
+    return L.circleMarker(latlng);
+  },
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup(feature.properties.name);
+  }
+}).addTo(map);
+
+function convertToHeatmapData(data) {
+  var heatmapData = [];
+  data.forEach(function(feature) {
+    var lat = feature.geometry.coordinates[1];
+    var lon = feature.geometry.coordinates[0];
+    var value = feature.properties.height;
+    heatmapData.push([lat, lon, value]);
+  });
+  return heatmapData;
 }
-// prepare spatial data for Leaflet heat
-function heatDataConvert(feature) {
- return [
- feature.geometry.coordinates[1],
- feature.geometry.coordinates[0],
- feature.properties.area,
- ]
+
+var heatmapData = convertToHeatmapData(cellTowers.toGeoJSON().features);
+
+var heatmap = L.heatLayer(heatmapData, {radius: 10}).addTo(map);
+
+function defaultMapSettings() {
+  map.setView([58.3781, 26.7295], 13);
 }
