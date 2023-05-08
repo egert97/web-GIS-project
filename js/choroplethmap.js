2c6f80cb6f2e1400088e9ae3efd88c367ff05911
egert97 <74450079+egert97@users.noreply.github.com>
2023-05-08 19:04:38 +0300

    Update choroplethmap.js

--- js/choroplethmap.js
+++ js/choroplethmap.js
@@ -1,19 +1,24 @@
 // create a new feature group
 const featureGroup = L.featureGroup().addTo(map);
 
-addGeoJson('geojson/tartu_city_districts_edu.geojson')
 // add geoJSON layer
-L.choropleth(data, {
+async function addGeoJson(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  const choroplethLayer = L.choropleth(data, {
     valueProperty: 'OBJECTID',
     scale: ['#ffffff', '#ff9900'],
     steps: 5,
     mode: 'q', // q for quantile, e for equidistant
     style: {
-    color: '#fff',
-    weight: 2,
-    fillOpacity: 0.8,
+      color: '#fff',
+      weight: 2,
+      fillOpacity: 0.8,
     },
     onEachFeature: function (feature, layer) {
-    layer.bindPopup('Value: ' + feature.properties.OBJECTID)
+      layer.bindPopup('Value: ' + feature.properties.OBJECTID);
     },
-    }).addTo(featureGroup)
+  });
+  featureGroup.addLayer(choroplethLayer);
+}
+addGeoJson('geojson/tartu_city_districts_edu.geojson');
