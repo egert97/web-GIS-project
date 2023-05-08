e75952fa699777636c8dbbf6bda51a45b053fef5
egert97 <74450079+egert97@users.noreply.github.com>
2023-05-08 20:38:13 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,4 +1,13 @@
+// create map
+var map = L.map('map').setView([58.38, 26.72], 12);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+ maxZoom: 18,
+}).addTo(map);
+
+// add geoJSON layer
 addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
 // add geoJSON layer
 async function addGeoJson(url) {
  const response = await fetch(url)
@@ -7,6 +16,7 @@ async function addGeoJson(url) {
  const heatMap = L.heatLayer(heatData, { radius: 10 })
  heatMap.addTo(map)
 }
+
 // prepare spatial data for Leaflet heat
 function heatDataConvert(feature) {
  const lat = feature.geometry.coordinates[1];
