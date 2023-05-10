44569949c042ed466f9dcb1bfac31b0ead93a050
egert97 <74450079+egert97@users.noreply.github.com>
2023-05-10 13:19:17 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,14 +1,5 @@
-// addGeoJson function to fetch and add GeoJSON layer
-async function addGeoJson(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    const heatData = data.features.map(heatDataConvert);
-    const heatMap = L.heatLayer(heatData, { radius: 10 });
-    heatMap.addTo(map);
-  }
-  
-  // Function to convert feature data for Leaflet heat
-  function heatDataConvert(feature) {
+// Function to convert feature data for Leaflet heat
+function heatDataConvert(feature) {
     return [
       feature.geometry.coordinates[1],
       feature.geometry.coordinates[0],
@@ -16,9 +7,28 @@ async function addGeoJson(url) {
     ];
   }
   
-  // Initialize the Leaflet map
-  const map = L.map('map').setView([0, 0], 2);
+  // Create a Leaflet map with default settings
+  function defaultMapSettings() {
+    const map = L.map('map').setView([58.38, 26.72], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors',
+      maxZoom: 18
+    }).addTo(map);
+    return map;
+  }
   
-  // Call addGeoJson function with the desired GeoJSON file
+  // Initialize the Leaflet map
+  const map = defaultMapSettings();
+  
+  // Add GeoJSON layer with heatmap
   addGeoJson('geojson/tartu_city_celltowers_edu.geojson');
+  
+  // Function to fetch and add GeoJSON layer
+  async function addGeoJson(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    const heatData = data.features.map(heatDataConvert);
+    const heatMap = L.heatLayer(heatData, { radius: 10 });
+    heatMap.addTo(map);
+  }
   
