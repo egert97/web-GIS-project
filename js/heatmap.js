a2116a8f0afd715988bc8386f72a0d913de3bef6
egert97 <74450079+egert97@users.noreply.github.com>
2023-05-10 13:06:44 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,17 +1,21 @@
-addGeoJson('geojson/tartu_city_celltowers_edu.geojson')
-// add geoJSON layer
+// addGeoJson function to fetch and add GeoJSON layer
 async function addGeoJson(url) {
- const response = await fetch(url)
- const data = await response.json()
- const heatData = data.features.map(heatDataConvert)
- const heatMap = L.heatLayer(heatData, { radius: 10 })
- heatMap.addTo(map)
-}
-// prepare spatial data for Leaflet heat
-function heatDataConvert(feature) {
- return [
- feature.geometry.coordinates[1],
- feature.geometry.coordinates[0],
- feature.properties.area,
- ]
-}
+    const response = await fetch(url);
+    const data = await response.json();
+    const heatData = data.features.map(heatDataConvert);
+    const heatMap = L.heatLayer(heatData, { radius: 10 });
+    heatMap.addTo(map);
+  }
+  
+  // Function to convert feature data for Leaflet heat
+  function heatDataConvert(feature) {
+    return [
+      feature.geometry.coordinates[1],
+      feature.geometry.coordinates[0],
+      feature.properties.area
+    ];
+  }
+  
+  // Call addGeoJson function with the desired GeoJSON file
+  addGeoJson('geojson/tartu_city_celltowers_edu.geojson');
+  
